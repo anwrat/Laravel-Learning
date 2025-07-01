04eb6a71c0be80fe34f87d4be32df66846d907ff
anwrat <anweshrwt22@gmail.com>
2025-07-01 07:11:40 +0545

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return 'This is users121212';
+});
