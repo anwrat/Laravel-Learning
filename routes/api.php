ff4339eaf304a9a1922ebde97ea149e109513ab9
anwrat <anweshrwt22@gmail.com>
2025-07-01 08:15:26 +0545

    Returning a json data through /api/users

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,21 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::get('/user', function (Request $request) {
-    return 'This is users121212';
-});
+Route::get('users',function(){
+    $users = [
+        [
+            'id'=>1,
+            'name' => 'John Doe',
+            'email'=>'john@example.com',
+            'is_verified'=>true,
+        ],
+        [
+            'id'=>2,
+            'name' => 'Jane Doe',
+            'email'=>'jane@example.com',
+            'is_verified'=>false,
+        ]];
+        //API should return to json, so should convert to json. Also can change status codes with this method
+        return response()->json($users,201);
+})
+?>
