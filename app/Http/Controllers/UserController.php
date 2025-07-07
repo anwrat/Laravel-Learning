1255440f20e54c29fb5671f71ec183cfaab1f84c
anwrat <anweshrwt22@gmail.com>
2025-07-07 07:52:25 +0545

    Making a controller for user and returning response to get api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = [
+            [
+                'id'=>1,
+                'name' => 'John Doe',
+                'email'=>'john@example.com',
+                'is_verified'=>true,
+            ],
+            [
+                'id'=>2,
+                'name' => 'Jane Doe',
+                'email'=>'jane@example.com',
+                'is_verified'=>false,
+            ]];
+        //API should return to json, so should convert to json. Also can change status codes with this method
+        return response()->json($users,201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,21 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::get('users',function(){
-    $users = [
-        [
-            'id'=>1,
-            'name' => 'John Doe',
-            'email'=>'john@example.com',
-            'is_verified'=>true,
-        ],
-        [
-            'id'=>2,
-            'name' => 'Jane Doe',
-            'email'=>'jane@example.com',
-            'is_verified'=>false,
-        ]];
-        //API should return to json, so should convert to json. Also can change status codes with this method
-        return response()->json($users,201);
-});
+Route::get('users',[UserController::class,'index']);
 
 Route::post('users',function(Request $request){
     //For only sending name
     //return $request->name;
     // return $request->all(); //For returning all key pairs from body
     $request->validate([
-        'name' => 'required'
+        'name' => 'required|min:3|max:10',
+        'email'=>'required_if:name,John',//Email is required only if name is john
     ]);
     return $request->all(); 
 });
