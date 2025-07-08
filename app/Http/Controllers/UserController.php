10f54399e86f913e65689247f65d9c385dfd550e
anwrat <anweshrwt22@gmail.com>
2025-07-08 07:47:09 +0545

    Making post API for creating users and get api for fetching users from database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,29 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserStoreRequest;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index(){
-        $users = [
-            [
-                'id'=>1,
-                'name' => 'John Doe',
-                'email'=>'john@example.com',
-                'is_verified'=>true,
-            ],
-            [
-                'id'=>2,
-                'name' => 'Jane Doe',
-                'email'=>'jane@example.com',
-                'is_verified'=>false,
-            ]];
+        // $users = [
+        //     [
+        //         'id'=>1,
+        //         'name' => 'John Doe',
+        //         'email'=>'john@example.com',
+        //         'is_verified'=>true,
+        //     ],
+        //     [
+        //         'id'=>2,
+        //         'name' => 'Jane Doe',
+        //         'email'=>'jane@example.com',
+        //         'is_verified'=>false,
+        //     ]];
+        
+        
+        //Using User models from app/Models/Users
+        $users = User::get()->all();
         //API should return to json, so should convert to json. Also can change status codes with this method
         return response()->json($users,201);
     }
 
     public function store(UserStoreRequest $request){
-        $user = $request->all();
+        $user = User::create($request->all());
         return response()->json($user,201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('users',[UserController::class,'index']);
 Route::post('users',[UserController::class,'store']);
 
-Route::post('users',function(Request $request){
-    //For only sending name
-    //return $request->name;
-    // return $request->all(); //For returning all key pairs from body
-    $request->validate([
-        'name' => 'required|min:3|max:10',
-        'email'=>'required_if:name,John',//Email is required only if name is john
-    ]);
-    return $request->all(); 
-});
+// Route::post('users',function(Request $request){
+//     //For only sending name
+//     //return $request->name;
+//     // return $request->all(); //For returning all key pairs from body
+//     $request->validate([
+//         'name' => 'required|min:3|max:10',
+//         'email'=>'required_if:name,John',//Email is required only if name is john
+//     ]);
+//     return $request->all(); 
+// });
 
 Route::put('users/{id}',function(Request $request, $id){
     return $id;
